ddb0b84bf4ea32569f5423f0fa522bb4557015ed
Marwan Dhiaur Rahman <45331803+marwandhiaurrahman@users.noreply.github.com>
2021-02-24 11:35:20 +0700

    delet asset md

--- resources/views/layouts/partials/css.blade.php
+++ resources/views/layouts/partials/css.blade.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" href="{{asset("assets/glyphicons/glyphicons.css")}}" type="text/css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/fontawesome.min.css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" />
-<link rel="stylesheet" href="{{asset("assets/material-design-icons/material-design-icons.css")}}" type="text/css" />
+{{-- <link rel="stylesheet" href="{{asset("assets/material-design-icons/material-design-icons.css")}}" type="text/css" /> --}}
 
 <link href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" rel="stylesheet">
 
