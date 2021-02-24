636bf3cd6bfd0cf730ce56ec358821937b2331ea
Marwan Dhiaur Rahman <45331803+marwandhiaurrahman@users.noreply.github.com>
2021-02-24 11:10:48 +0700

    perbaikan material design

--- resources/views/layouts/partials/css.blade.php
+++ resources/views/layouts/partials/css.blade.php
@@ -5,6 +5,8 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css" />
 <link rel="stylesheet" href="{{asset("assets/material-design-icons/material-design-icons.css")}}" type="text/css" />
 
+<link href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" rel="stylesheet">
+
 <link rel="stylesheet" href="{{asset("assets/bootstrap/dist/css/bootstrap.min.css")}}" type="text/css" />
 <!-- build:css assets/styles/app.min.css -->
 <link rel="stylesheet" href="{{asset("assets/styles/app.css")}}" type="text/css" />
